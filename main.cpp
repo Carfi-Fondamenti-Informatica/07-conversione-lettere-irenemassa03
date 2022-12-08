54aaf3ebfa2be41c5f4c09dda26521e043aa1034
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-12-08 19:06:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char x;
+  cin >> x;
+  if (convL(x)){
+    cout << x <<endl;
+  }
+  else{
+    cout << "errore" << endl;
+  }
   return 0;
 }
