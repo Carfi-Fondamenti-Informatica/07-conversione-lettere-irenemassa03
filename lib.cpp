832ee7e3e88bc0abb90ef2df2bb1d71c7c30c13e
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-12-08 19:04:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+bool convL(char &a){
+  if ((a>=65 and a<=90) or (a>=97 and a<=122)){
+    if (a>=65 and a <=90){
+      a+=32;
+    }
+    else{
+      a-=32;
+    }
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+  
+ 
 
